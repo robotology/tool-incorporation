0dfa705b78a035c7223dbc1e01bf4e105b2dc34e
iCub <robotcub-hackers@lists.sourceforge.net>
2017-10-02 12:49:04 +0200

    added a small comment in CMakeLists, linking to bug fix for PCL compilation

--- modules/toolFeatExt/include/Point3D.h
+++ modules/toolFeatExt/include/Point3D.h
@@ -41,10 +41,10 @@ public:
   }
 
   // read and write structure on a connection
-  bool read(yarp::os::idl::WireReader& reader) YARP_OVERRIDE;
-  bool read(yarp::os::ConnectionReader& connection) YARP_OVERRIDE;
-  bool write(yarp::os::idl::WireWriter& writer) YARP_OVERRIDE;
-  bool write(yarp::os::ConnectionWriter& connection) YARP_OVERRIDE;
+  bool read(yarp::os::idl::WireReader& reader) override;
+  bool read(yarp::os::ConnectionReader& connection) override;
+  bool write(yarp::os::idl::WireWriter& writer) override;
+  bool write(yarp::os::ConnectionWriter& connection) override;
 
 private:
   bool write_x(yarp::os::idl::WireWriter& writer);
@@ -148,8 +148,8 @@ public:
     void clean() {
       dirty_flags(false);
     }
-    bool read(yarp::os::ConnectionReader& connection) YARP_OVERRIDE;
-    bool write(yarp::os::ConnectionWriter& connection) YARP_OVERRIDE;
+    bool read(yarp::os::ConnectionReader& connection) override;
+    bool write(yarp::os::ConnectionWriter& connection) override;
   private:
 
     Point3D *obj;
